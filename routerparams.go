ebbbfa1ea17c5fb71934713ccd1694df9c6bd5a9
roc.wong <admin@rocwong.com>
2015-07-01 11:45:34 +0800

    Add parameter to get json data

--- routerparams.go
+++ routerparams.go
@@ -1,9 +1,11 @@
 package neko
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"mime/multipart"
 	"net/http"
+	"io/ioutil"
 )
 
 type routerParams struct {
@@ -25,4 +27,31 @@ func (c *routerParams) ByPost(name string) string {
 
 func (c *routerParams) File(name string) (multipart.File, *multipart.FileHeader, error) {
 	return c.req.FormFile(name)
+}
+
+func (c *routerParams) Json() *jsonParams {
+	defer c.req.Body.Close()
+
+	data, _ := ioutil.ReadAll(c.req.Body)
+	objJson := &jsonParams{ data: map[string]string{}}
+	objJson.source = string(data)
+	json.Unmarshal(data, &objJson.data);
+
+	return objJson
+}
+
+type jsonParams struct {
+	source string
+	data map[string]string
+}
+
+func (c *jsonParams) Get(name string) string {
+	if len(c.data) == 0 {
+		return ""
+	}
+	return c.data[name]
+}
+
+func (c *jsonParams) String() string {
+	return c.source
 }
